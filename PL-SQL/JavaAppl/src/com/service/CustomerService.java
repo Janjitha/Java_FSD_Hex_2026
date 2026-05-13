d074e0b4aabeadbeec24e37a5cf0830b14642621
Janjitha <janjitha412@gmail.com>
2026-05-13 23:07:10 +0530

    pl-sql-first-procedure-call

---
+++ PL-SQL/JavaAppl/src/com/service/CustomerService.java
@@ -0,0 +1,20 @@
+package com.service;
+
+import com.model.Customer;
+import com.repository.CustomerRepository;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class CustomerService {
+
+    CustomerRepository customerRepository = new CustomerRepository();
+
+    public List<Customer> getAllCustomers() {
+        return customerRepository.getAllCustomers();
+    }
+
+    public List<Customer> getCustomersByCity(String city) throws SQLException {
+        return customerRepository.getCustomersByCity(city);
+    }
+}
