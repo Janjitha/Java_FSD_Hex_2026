3305973fd85260c7dc52819c8e5954321f625d6f
Janjitha <janjitha412@gmail.com>
2026-06-08 00:35:55 +0530

    React Fundamentals Ex-1 useState and Arithmetic Operations

--- PL-SQL/MyReactApp/src/App.jsx
+++ PL-SQL/MyReactApp/src/App.jsx
@@ -1,14 +1,30 @@
+/*
 //this is the content which appears finally
-
 function App(){  // This is a react component
- 
-
  return ( // Every react component, must return a JSX : HTML + CSS + Javascript 
   <div>
       <h1>Hello React</h1>
   </div>
  )
-
 }
 // if we are importing the app in the main.jsx then sure we need to export here
  export default App;
+ */
+//  -------------------------------------------------------------------------------
+import ArithmeticComponent from "./components/ArithmeticComponent";
+function App(){ // This is the Parent component in react 
+
+  return(  // This return fn has to be there in every react component. It returns JSX
+    // <div>
+    //   <h1>Hello React</h1>
+    // </div>
+    <div>
+       <ArithmeticComponent />
+    </div>
+  )
+}
+export default App; 
+// This is mandatory. If we dont export the fn, we cannot import it anywhere else
+/*
+JSX : JavaScript Extension (HTML + CSS) : HTML + CSS + JavaScript 
+*/
